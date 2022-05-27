d8c619c472a7000dd1ef982869286cade400fa8e
Александр <vxtoimg@rambler.ru>
2022-05-27 20:40:08 +0300

    Add .vscode dir , VSCode proj setting

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>   
 
+ #include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int ddd = 5050;
+    char * st = "ssstst2";
+    strlen(st);
+
+    printf("Hellow World ");
+    return 0;
+}
+ 
